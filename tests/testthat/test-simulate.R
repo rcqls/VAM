43b003ca2e26d7b36d81db2b0d7fd27a64fd02d7
ldoyen <laurent.doyen@iut2.upmf-grenoble.fr>
2016-05-04 13:11:41 +0200

    fix data list format

--- tests/testthat/test-simulate.R
+++ tests/testthat/test-simulate.R
@@ -2,11 +2,10 @@
 context("simulate")
 
 test_that("Weibull",{
-	theta<-c(0.3,2.4,0.6)	
-	sim <- sim.vam( ~ (ARAInf(theta[3]) | Weibull(theta[1],theta[2])))
+	sim <- sim.vam( ~ (ARAInf(0.6) | Weibull(0.3,2.4)))
 	set.seed(0.5)
 	U<-runif(1)
-	Tcour<-(-(theta[1])^(-1)*log(U[1]))^(1/theta[2])
+	Tcour<-(-(0.3)^(-1)*log(U[1]))^(1/2.4)
 	Data<-data.frame(Time=Tcour,Type=c(-1))
 	set.seed(0.5)
 	expect_that(simulate(sim,1),equals(Data,tolerance=0.00000000000001))
@@ -14,7 +13,7 @@ test_that("Weibull",{
 	names(Data_list)<-"System1"
 	set.seed(0.5)
 	expect_that(simulate(sim,1,as.list=TRUE),equals(Data_list,tolerance=0.00000000000001))
-	sim2 <- sim.vam( T & C ~ (ARAInf(theta[3]) | Weibull(theta[1],theta[2])))
+	sim2 <- sim.vam( T & C ~ (ARAInf(0.6) | Weibull(0.3,2.4)))
 	Data2<-data.frame(T=Tcour,C=c(-1))
 	set.seed(0.5)
 	expect_that(simulate(sim2,1),equals(Data2,tolerance=0.00000000000001))
